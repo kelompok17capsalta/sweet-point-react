a6f4b55287a38c7546fc8c2ca8c8b182463498ee
Yusril A. P <yusriladr.37@gmail.com>
2022-06-06 15:19:07 +0700

    Add @popperjs/core

--- src/pages/customer/Home/index.jsx
+++ src/pages/customer/Home/index.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
+import styles from './style.module.css';
 
 const Home = () => {
   const customer = useSelector((state) => state.customer.value);
 
   console.log(customer);
   return (
-    <div>
+    <div className={`container ${styles.container_custom}`}>
       Hello World.
     </div>
   );
