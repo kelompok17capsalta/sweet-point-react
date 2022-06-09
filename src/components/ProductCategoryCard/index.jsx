1acfbdd8f54ece6e7baa50631776d72e51919f9b
Agus Pranata <aguspranata665@gmail.com>
2022-06-09 10:19:00 +0800

    [feature] add components for redeem page

---
+++ src/components/ProductCategoryCard/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./style.module.css";
+
+const ProductCategoryCard = ({ image, title, redirectTo }) => {
+  return (
+    <Link to={redirectTo}>
+      <div className={`${styles.product_category_container}`}>
+        <div height="60px">
+          <img src={image} height="100%" Ak />
+        </div>
+        <span className={`mt-2 ${styles.product_category_title}`}>{title}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCategoryCard;
