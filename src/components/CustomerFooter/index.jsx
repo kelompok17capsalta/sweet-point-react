19b180e0a1d879cc39339f0687e6daf68ee7a2b8
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-06-14 19:19:30 +0700

    Complete customer footer

---
+++ src/components/CustomerFooter/index.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import styles from './style.module.css';
+
+const Footer = () => (
+  <footer className={`py-3 ${styles.footer}`}>
+    <div className="container">
+      <div className="d-flex flex-column flex-lg-row align-items-lg-center justify-content-between">
+        <div className="d-flex align-items-center">
+            <img src="/logo.png" alt="Logo" width="60" height="60" />
+            <span className={`text-light h4 ${styles.title} mt-1`}>Sweet Point</span>
+        </div>
+
+        <div className="d-flex align-items-center m-auto m-lg-0">
+          <a href="/" className={`btn btn-outline-light rounded-circle ${styles.social_icon}`}>
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a href="/" className={`btn btn-outline-light rounded-circle mx-3 ${styles.social_icon}`}>
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a href="/" className={`btn btn-outline-light rounded-circle ${styles.social_icon}`}>
+            <i className="bi bi-instagram"></i>
+          </a>
+        </div>
+      </div>
+
+      <hr className="text-light" />
+
+      <p className="h4 text-light">Sweet Point</p>
+      <div className="d-flex flex-column flex-lg-row align-items-lg-center justify-content-between mb-3">
+        <ul className={`d-flex flex-column flex-lg-row align-items-lg-center p-0 mt-3 ${styles.footer_nav}`}>
+          <li>
+            <Link to="/" className="text-light text-decoration-none">All Shops</Link>
+          </li>
+
+          <li>
+            <Link to="/" className="text-light text-decoration-none mx-lg-3">About Us</Link>
+          </li>
+
+          <li>
+            <Link to="/help" className="text-light text-decoration-none">FAQ</Link>
+          </li>
+        </ul>
+
+        <a href="/" target="__blank" className="btn btn-outline-light">Contact Us</a>
+      </div>
+
+      <p className="text-center text-light">© Sweet Point 2022 All rights reserved</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
