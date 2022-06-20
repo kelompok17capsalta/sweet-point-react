1b3557c204774a9b52d74549dfd1174197d65404
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-06-20 20:33:01 +0700

    Config redirects for prod API

--- src/global/API_ENDPOINT.js
+++ src/global/API_ENDPOINT.js
@@ -2,13 +2,13 @@ import CONFIG from "./CONFIG";
 
 const API_ENDPOINT = {
   CUSTOMER: {
-    SIGN_UP: `${CONFIG.API_BASE_URL}/auth/register`,
-    SIGN_IN: `${CONFIG.API_BASE_URL}/auth/login`,
-    INFO: `${CONFIG.API_BASE_URL}/auth/info`,
+    SIGN_UP: `${CONFIG.API_BASE_URL}/v1/auth/register`,
+    SIGN_IN: `${CONFIG.API_BASE_URL}/v1/auth/login`,
+    INFO: `${CONFIG.API_BASE_URL}/v1/auth/info`,
   },
   ADMIN: {
-    SIGN_IN: `${CONFIG.API_BASE_URL}/auth/login`,
-    INFO: `${CONFIG.API_BASE_URL}/auth/info`,
+    SIGN_IN: `${CONFIG.API_BASE_URL}/v1/auth/login`,
+    INFO: `${CONFIG.API_BASE_URL}/v1/auth/info`,
   },
 };
 
