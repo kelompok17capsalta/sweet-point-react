1bbe4826ac197794d795017f63c78f477f5e9cc2
Agus86-cell <aguspranata665@gmail.com>
2022-06-21 23:56:22 +0800

    feature homepage

--- src/pages/customer/Home/index.jsx
+++ src/pages/customer/Home/index.jsx
@@ -1,13 +1,22 @@
-import { useSelector } from 'react-redux';
-import styles from './style.module.css';
+import { useSelector } from "react-redux";
+import Banner from "../../../components/CostumerBanner";
+import Section1 from "../../../components/HomeSection/Section1";
+import Section2 from "../../../components/HomeSection/Section2";
+import Section3 from "../../../components/HomeSection/Section3";
+import styles from "./style.module.css";
 
 const Home = () => {
   const customer = useSelector((state) => state.customer.value);
 
   console.log(customer);
   return (
-    <div className={`${styles.container_custom}`}>
-      Hello World.
+    <div className="container py-3">
+      <div>
+        <Banner />
+      </div>
+      <Section1 />
+      <Section3 />
+      <Section2 />
     </div>
   );
 };
