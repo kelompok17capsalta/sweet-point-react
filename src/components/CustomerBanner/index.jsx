f3587cfc671823a7f5d711af229961625965e676
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-06-22 14:24:11 +0700

    Change home banner with swipper

--- src/components/CustomerBanner/index.jsx
+++ src/components/CustomerBanner/index.jsx
@@ -1,82 +1,40 @@
+import { useState } from 'react';
+import { Navigation, Pagination, Controller } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import bannerFile1 from './banner/banner-1.jpg';
+import styles from './style.module.css';
 
 export default function Banner() {
+  const [controlledSwiper, setControlledSwiper] = useState(null);
+
+  const handlePrevSlide = () => {
+    controlledSwiper?.slidePrev();
+  };
+
+  const handleNextSlide = () => {
+    controlledSwiper?.slideNext();
+  };
+
   return (
     <div>
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide"
-        data-bs-ride="true"
-      >
-        <div className="carousel-indicators" style={{ height: "10px" }}>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div className="carousel-inner center">
-          <div className="carousel-item active">
-            <img
-              src={bannerFile1}
-              className="d-block w-100"
-              alt="banner"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src={bannerFile1}
-              className="d-block w-100"
-              alt="banner"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src={bannerFile1}
-              className="d-block w-100"
-              alt="banner"
-            />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
+      <div className="position-relative" style={{ maxWidth: '100%'}}>
+        <span tabIndex={0} className={`h2 text-primary bi bi-arrow-left-circle-fill ${styles.navigation} ${styles.navigation__prev}`} onClick={handlePrevSlide} />
+        <Swiper
+          modules={[Navigation, Pagination, Controller]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          controller={{ control: controlledSwiper }}
+          onSwiper={setControlledSwiper}
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+          {[1,2,3,4].map(() => (
+            <SwiperSlide>
+              <img src={bannerFile1} alt="banner" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <span tabIndex={0} className={`h2 text-primary bi bi-arrow-right-circle-fill ${styles.navigation} ${styles.navigation__next}`} onClick={handleNextSlide} />
       </div>
     </div>
   );
