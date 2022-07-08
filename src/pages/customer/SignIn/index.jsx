2882cf1782704e694db7ea5501f9610fb518aaeb
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-07-08 22:31:22 +0700

    Add required into login input

--- src/pages/customer/SignIn/index.jsx
+++ src/pages/customer/SignIn/index.jsx
@@ -78,13 +78,13 @@ const SignIn = () => {
                   <label className="mb-1 px-1">
                     <h4 className={styles.color_text}>Username</h4>
                   </label>
-                  <input className="mb-4" type="text" placeholder="Username" name="username" onChange={handleChange} />
+                  <input className="mb-4" type="text" placeholder="Username" name="username" onChange={handleChange} required />
                 </div>
                 <div className="row mb-3 px-3">
                   <label className="mb-0 px-1">
                     <h4 className={styles.color_text}>Password</h4>
                   </label>
-                  <input className={`mb-1`} type={clickEye ? "text" : "password"} placeholder="Password" name="password" onChange={handleChange} />
+                  <input className={`mb-1`} type={clickEye ? "text" : "password"} placeholder="Password" name="password" onChange={handleChange} required />
                   <div className={` ${styles.col_layout} col-auto`}>
                     <i className={clickEye ? `bi bi-eye` : `bi bi-eye-slash`} onClick={toggleEye}></i>
                   </div>
