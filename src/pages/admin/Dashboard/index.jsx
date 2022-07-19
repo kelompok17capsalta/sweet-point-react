f03020665ff1b1f48667b2e0acb6a66740addc9a
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-07-19 15:17:20 +0700

    Fix eslint style

--- src/pages/admin/Dashboard/index.jsx
+++ src/pages/admin/Dashboard/index.jsx
@@ -1,24 +1,22 @@
-import styles from "./style.module.css";
+import styles from './style.module.css';
 
-import TableTransactionAdmin from "../../../components/TableTransactionAdmin";
-import TotalPoint from "../../../components/TotalPoint";
+import TableTransactionAdmin from '../../../components/TableTransactionAdmin';
+import TotalPoint from '../../../components/TotalPoint';
 
-const Dashboard = () => {
-	return (
-		<div className={`container mt-5 ${styles.container}`}>
-			<span className={styles.header}>Welcome back, Admin!</span>
-			<div className="d-flex justify-content-center flex-wrap">
-				<TotalPoint />
-				<TotalPoint />
-				<TotalPoint />
-			</div>
+const Dashboard = () => (
+  <div className={`container mt-5 ${styles.container}`}>
+    <span className={styles.header}>Welcome back, Admin!</span>
+    <div className="d-flex justify-content-center flex-wrap">
+      <TotalPoint />
+      <TotalPoint />
+      <TotalPoint />
+    </div>
 
-			<div className="container mt-5">
-				<span className={styles.transaction}>Pending Transaction</span>
-				<TableTransactionAdmin />
-			</div>
-		</div>
-	);
-};
+    <div className="container mt-5">
+      <span className={styles.transaction}>Pending Transaction</span>
+      <TableTransactionAdmin />
+    </div>
+  </div>
+);
 
 export default Dashboard;
