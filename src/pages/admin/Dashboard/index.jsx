3e6f6f02fde4856c3333ae4e07eb46a776b50383
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-07-19 20:18:49 +0700

    Add admin summary

--- src/pages/admin/Dashboard/index.jsx
+++ src/pages/admin/Dashboard/index.jsx
@@ -1,16 +1,12 @@
 import styles from './style.module.css';
 
 import TableTransactionAdmin from '../../../components/TableTransactionAdmin';
-import TotalPoint from '../../../components/TotalPoint';
+import AdminSummary from '../../../components/AdminSummary';
 
 const Dashboard = () => (
   <div className={`container mt-5 ${styles.container}`}>
     <span className={styles.header}>Welcome back, Admin!</span>
-    <div className="d-flex justify-content-center flex-wrap">
-      <TotalPoint />
-      <TotalPoint />
-      <TotalPoint />
-    </div>
+    <AdminSummary />
 
     <div className="container mt-5">
       <span className={styles.transaction}>Pending Transaction</span>
