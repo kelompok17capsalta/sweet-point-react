7caaf8990213a4943a914235ab232556a63e8c4d
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-07-19 15:11:46 +0700

    Fix eslint style

--- src/components/PopularRedeem/index.jsx
+++ src/components/PopularRedeem/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Navigation, Pagination, Controller } from 'swiper';
+// eslint-disable-next-line import/no-unresolved
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import styles from './style.module.css';
@@ -73,34 +74,34 @@ const PopularRedeem = () => {
     <div className={`card ${styles.card_bg} p-3`}>
       <p className="h4 card-title">Popular Redeem</p>
 
-      <div className="position-relative" style={{ maxWidth: '100%'}}>
+      <div className="position-relative" style={{ maxWidth: '100%' }}>
         <span tabIndex={0} className={`h2 text-primary bi bi-arrow-left-circle-fill ${styles.navigation} ${styles.navigation__prev}`} onClick={handlePrevSlide} />
         <Swiper
           modules={[Navigation, Pagination, Controller]}
           breakpoints={{
             0: {
               slidesPerView: 1,
-              spaceBetween: 10
+              spaceBetween: 10,
             },
             768: {
               slidesPerView: 3,
-              spaceBetween: 30
+              spaceBetween: 30,
             },
             992: {
               slidesPerView: 4,
-              spaceBetween: 40
-            }
+              spaceBetween: 40,
+            },
           }}
           pagination={{ clickable: true }}
           scrollbar={{ draggable: true }}
           controller={{ control: controlledSwiper }}
           onSwiper={setControlledSwiper}
         >
-         {redeems.map((redeem, redeemIdx) => (
-          <SwiperSlide key={redeemIdx}>
-            <PopularRedeemItem {...redeem} />
-          </SwiperSlide>
-         ))}
+          {redeems.map((redeem, redeemIdx) => (
+            <SwiperSlide key={redeemIdx}>
+              <PopularRedeemItem {...redeem} />
+            </SwiperSlide>
+          ))}
         </Swiper>
         <span tabIndex={0} className={`h2 text-primary bi bi-arrow-right-circle-fill ${styles.navigation} ${styles.navigation__next}`} onClick={handleNextSlide} />
       </div>
